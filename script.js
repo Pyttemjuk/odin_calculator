9dbb074c9ac91d71ba11beeb418cd5c99a6f7e18
Emilia Engberg <engberg.emilia@gmail.com>
2023-02-08 10:57:51 +0100

    Add basic math operators (add, subtract, ...)

---
+++ script.js
@@ -0,0 +1,6 @@
+const Add = (number1, number2) => number1 + number2
+const Subtract = (number1, number2) => number1 - number2
+const Divide = (number1, number2) => number1 / number2
+const Multiply = (number1, number2) => number1 * number2
+
+const operate = (operator, number1, number2) => operator(number1, number2)
